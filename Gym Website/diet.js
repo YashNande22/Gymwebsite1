f6c4d1c5fe09d66882b3fa9941fe296b70af0f81
siddhantmisal50 <siddhantmisal50@gmail.com>
2023-05-06 21:06:22 +0530

    Completed remaining work

--- Gym Website/diet.js
+++ Gym Website/diet.js
@@ -1,29 +1,31 @@
-const form = document.querySelector('form');
-const foodSelect = document.querySelector('#food-select');
-const caloriesInput = document.querySelector('#calories-input');
-const dietList = document.querySelector('#diet-list');
+const form = document.querySelector("form");
+const foodSelect = document.querySelector("#food-select");
+const caloriesInput = document.querySelector("#calories-input");
+const dietList = document.querySelector("#diet-list");
 
 const MAX_CALORIES = 1500; // set the maximum caloric value for the diet list
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const foodOption = foodSelect.options[foodSelect.selectedIndex];
   const foodName = foodOption.value;
-  const foodCalories = parseInt(foodOption.getAttribute('data-calories'));
+  const foodCalories = parseInt(foodOption.getAttribute("data-calories"));
   const caloriesToAdd = parseInt(caloriesInput.value);
 
   const totalCalories = calculateTotalCalories();
   const remainingCalories = MAX_CALORIES - totalCalories;
 
   if (caloriesToAdd <= remainingCalories) {
-    const listItem = document.createElement('li');
+    const listItem = document.createElement("li");
     listItem.textContent = `${foodName} (${caloriesToAdd} calories)`;
     dietList.appendChild(listItem);
 
-    caloriesInput.value = '';
+    caloriesInput.value = "";
   } else {
-    alert(`You can only add up to ${remainingCalories} calories to your diet list.`);
+    alert(
+      `You can only add up to ${remainingCalories} calories to your diet list.`
+    );
   }
 });
 
